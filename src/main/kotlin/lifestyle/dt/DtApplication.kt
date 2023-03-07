cf5669397d4a2799c4f14a5ba7e3ff071f17a15a
aesperer <piyrw9754@gmail.com>
2023-03-07 10:07:46 +0900

    :tada: Start Project

---
+++ src/main/kotlin/lifestyle/dt/DtApplication.kt
@@ -0,0 +1,11 @@
+package lifestyle.dt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DtApplication
+
+fun main(args: Array<String>) {
+	runApplication<DtApplication>(*args)
+}
