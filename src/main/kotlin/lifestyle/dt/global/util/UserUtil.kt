6595c1609bd634ad0b359644f0e430d668a4d721
aesperer <piyrw9754@gmail.com>
2023-03-18 19:40:25 +0900

    :recycle: username을 uuid로 설정

--- src/main/kotlin/lifestyle/dt/global/util/UserUtil.kt
+++ src/main/kotlin/lifestyle/dt/global/util/UserUtil.kt
@@ -4,9 +4,11 @@ import lifestyle.dt.domain.user.domain.User
 import lifestyle.dt.domain.user.domain.repository.UserRepository
 import lifestyle.dt.domain.user.exception.UserNotFoundException
 import lifestyle.dt.global.security.auth.AuthDetails
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
 class UserUtil(
@@ -15,14 +17,14 @@ class UserUtil(
 
     fun fetchCurrentUser(): User {
         val principal = SecurityContextHolder.getContext().authentication.principal
-        val email = if (principal is UserDetails) {
+        val uuid = if (principal is UserDetails) {
             (principal as AuthDetails).username
         } else {
             principal.toString()
         }
-        return fetchUserByEmail(email)
+        return fetchUserByEmail(uuid)
     }
 
-    fun fetchUserByEmail(email: String): User =
-        userRepository.findByEmail(email) ?: throw UserNotFoundException()
+    fun fetchUserByEmail(uuid: String): User =
+        userRepository.findByIdOrNull(UUID.fromString(uuid)) ?: throw UserNotFoundException()
 }
