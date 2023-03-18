be6091402a235b06fed1c7f216556b4627f83975
aesperer <piyrw9754@gmail.com>
2023-03-18 19:50:10 +0900

    :recycle: filter config parser 인자로 설정

--- src/main/kotlin/lifestyle/dt/global/config/FilterConfig.kt
+++ src/main/kotlin/lifestyle/dt/global/config/FilterConfig.kt
@@ -4,18 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import lifestyle.dt.global.filter.ExceptionFilter
 import lifestyle.dt.global.filter.JwtTokenFilter
 import lifestyle.dt.global.security.jwt.JwtGenerator
+import lifestyle.dt.global.security.jwt.JwtParser
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 class FilterConfig(
-    private val jwtTokenProvider: JwtGenerator,
+    private val jwtParser: JwtParser
     private val objectMapper: ObjectMapper
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain? , HttpSecurity>() {
 
     override fun configure(builder: HttpSecurity) {
-        val jwtTokenFilter = JwtTokenFilter(jwtTokenProvider)
+        val jwtTokenFilter = JwtTokenFilter(jwtParser)
         val exceptionFilter = ExceptionFilter(objectMapper)
         builder.addFilterBefore(exceptionFilter, UsernamePasswordAuthenticationFilter::class.java)
         builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
--- src/main/kotlin/lifestyle/dt/global/security/SecurityConfig.kt
+++ src/main/kotlin/lifestyle/dt/global/security/SecurityConfig.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import lifestyle.dt.global.config.FilterConfig
 import lifestyle.dt.global.security.handler.CustomAuthenticationEntryPoint
 import lifestyle.dt.global.security.jwt.JwtGenerator
+import lifestyle.dt.global.security.jwt.JwtParser
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -17,7 +18,8 @@ import org.springframework.web.cors.CorsUtils
 @EnableWebSecurity
 class SecurityConfig(
     private val objectMapper: ObjectMapper,
-    private val jwtGenerator: JwtGenerator
+    private val jwtParser: JwtParser
+
 ) {
 
     @Bean
@@ -44,7 +46,7 @@ class SecurityConfig(
             .authenticationEntryPoint(CustomAuthenticationEntryPoint(objectMapper))
 
             .and()
-            .apply(FilterConfig(jwtGenerator, objectMapper))
+            .apply(FilterConfig(jwtParser, objectMapper))
 
             .and()
             .build()
