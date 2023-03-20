1e370f4ae51076dd99ef727e9874057828f596c3
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-20 13:56:28 +0900

    :sparkles: :: 이메일 중복 예외 처리 추가

--- src/main/kotlin/lifestyle/dt/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/lifestyle/dt/domain/user/domain/repository/UserRepository.kt
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface UserRepository : JpaRepository<User, UUID> {
+
     fun findByEmail(email: String?): User?
+    fun existsByEmail(email: String): Boolean
+
 }
