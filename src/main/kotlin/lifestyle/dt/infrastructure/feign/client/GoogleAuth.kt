12413c46185b19a90d00a436c9048c9d9c564dc7
aesperer <piyrw9754@gmail.com>
2023-03-28 20:35:53 +0900

    :sparkles: :: GoogleAuth Info

--- src/main/kotlin/lifestyle/dt/infrastructure/feign/client/GoogleAuth.kt
+++ src/main/kotlin/lifestyle/dt/infrastructure/feign/client/GoogleAuth.kt
@@ -1,7 +1,14 @@
 package lifestyle.dt.infrastructure.feign.client
 
+import lifestyle.dt.infrastructure.feign.dto.request.GoogleCodeRequest
+import lifestyle.dt.infrastructure.feign.dto.response.GoogleTokenResponse
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.PostMapping
 
 @FeignClient(name = "GoogleAuthClient", url = "https://oauth2.googleapis.com/token")
-class GoogleAuth {
+interface GoogleAuth {
+
+    @PostMapping
+    fun googleAuth(googleCodeRequest: GoogleCodeRequest): GoogleTokenResponse
+
 }
--- src/main/kotlin/lifestyle/dt/infrastructure/feign/client/GoogleInfo.kt
+++ src/main/kotlin/lifestyle/dt/infrastructure/feign/client/GoogleInfo.kt
@@ -1,7 +1,12 @@
 package lifestyle.dt.infrastructure.feign.client
 
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @FeignClient(name = "GoogleInfoClient", url = "https://www.googleapis.com/oauth2/v1/userinfo")
-class GoogleInfo {
+interface GoogleInfo {
+
+    @GetMapping("?alt=json&access_token={ACCESS_TOKEN}")
+    fun googleInfo(@PathVariable("ACCESS_TOKEN") accessToken: String)
 }
