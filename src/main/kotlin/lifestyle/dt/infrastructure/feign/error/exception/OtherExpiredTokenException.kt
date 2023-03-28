d8a5c0ec04c73c48f1b0127c0a6795e17e63b3cf
aesperer <piyrw9754@gmail.com>
2023-03-28 21:01:18 +0900

    :pencil2: Expried Token -> Other

--- src/main/kotlin/lifestyle/dt/infrastructure/feign/error/exception/OtherExpiredTokenException.kt
+++ src/main/kotlin/lifestyle/dt/infrastructure/feign/error/exception/OtherExpiredTokenException.kt
@@ -3,4 +3,4 @@ package lifestyle.dt.infrastructure.feign.error.exception
 import lifestyle.dt.global.error.ErrorCode
 import lifestyle.dt.global.error.exception.DTException
 
-class OtherExpiredTokenException : DTException(ErrorCode.EXPIRED_TOKEN)
+class OtherExpiredTokenException : DTException(ErrorCode.OTHER_EXPIRED_TOKEN)
