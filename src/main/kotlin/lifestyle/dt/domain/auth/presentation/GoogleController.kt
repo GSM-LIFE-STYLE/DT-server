d0288ae6a7e0cfa6327ede29e18e52325bcc379f
aesperer <piyrw9754@gmail.com>
2023-03-29 15:02:27 +0900

    :sparkles: google code redirect api

--- src/main/kotlin/lifestyle/dt/domain/auth/presentation/GoogleController.kt
+++ src/main/kotlin/lifestyle/dt/domain/auth/presentation/GoogleController.kt
@@ -2,6 +2,7 @@ package lifestyle.dt.domain.auth.presentation
 
 import lifestyle.dt.domain.auth.presentation.data.response.TokenResponse
 import lifestyle.dt.domain.auth.service.GoogleAuthService
+import lifestyle.dt.domain.auth.service.QueryGoogleAuthLinkService
 import lifestyle.dt.domain.user.util.UserConverter
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class GoogleController(
     private val googleAuthService: GoogleAuthService,
+    private val googleQueryGoogleAuthLinkService: QueryGoogleAuthLinkService,
     private val userConverter: UserConverter
 ) {
+
+    @GetMapping("/google/auth")
+    fun queryGoogleAuthLink(): ResponseEntity<String> =
+        googleQueryGoogleAuthLinkService.execute()
+            .let { ResponseEntity.ok(it) }
+
 
     @GetMapping("/receive-code")
     fun googleAuthLogin(@RequestParam("code") code: String): ResponseEntity<TokenResponse> =
