5392fc3494f5a35337e7151f9c41e439284b084e
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-04-03 11:26:23 +0900

    :recycle: :: resolveToken -> resolveAccessToken으로 변경

--- src/main/kotlin/lifestyle/dt/global/filter/JwtTokenFilter.kt
+++ src/main/kotlin/lifestyle/dt/global/filter/JwtTokenFilter.kt
@@ -1,6 +1,5 @@
 package lifestyle.dt.global.filter
 
-import lifestyle.dt.global.security.jwt.JwtGenerator
 import lifestyle.dt.global.security.jwt.JwtParser
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
@@ -17,7 +16,7 @@ class JwtTokenFilter(
         response: HttpServletResponse,
         filterChain: FilterChain,
     ) {
-        val token: String? = jwtParser.resolveToken(request)
+        val token: String? = jwtParser.resolveAccessToken(request)
         if (!token.isNullOrBlank()) {
             val authentication = jwtParser.authentication(token)
             SecurityContextHolder.getContext().authentication = authentication
